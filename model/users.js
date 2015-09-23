b644bfb35a31413d767dca4b3a23265eaa84c6ed
Simon <svansintjan@gmail.com>
2015-09-23 09:19:51 -0800

    Remove quick-survey specific code

--- model/users.js
+++ model/users.js
@@ -4,14 +4,3 @@ Meteor.users.allow({
     return (userId && userId === user._id) || Meteor.user().is_admin;
   },
 });
-
-Meteor.users.deny({
-  update: function(userId, user, fieldnames, modifier) {
-    if (user.has_submitted === true &&
-        _.contains(fieldnames, 'has_submitted') && !Meteor.user().is_admin) {
-      return true;
-    }
-    return false;
-  }
-});
-
