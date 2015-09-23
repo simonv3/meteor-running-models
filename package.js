9b7ac22be58069fa3b5dde651b7d10f0fd8a1aef
Simon <svansintjan@gmail.com>
2015-09-23 09:20:30 -0800

    Bump patch number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'simonv3:meteor-running-models',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'The models package for Meteor Running',
   // URL to the Git repository containing the source code for this package.
